d5554a6cae3555a7d104aca633c015482e9b5b58
Yohan Kim <ddalpange@gmail.com>
2024-06-02 20:44:01 +0900

    deltaTime 추가

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,13 @@ import "./style.css";
 new p5((p5Instance) => {
   const p = p5Instance as unknown as p5;
   window.p = p;
+  let deltaTime = 0;
+  let lastMills = 0;
+  window.deltaTime = deltaTime;
+  window.updateDeltaTime = () => {
+    const currentMills = Date.now();
+    deltaTime = (currentMills - lastMills) / 1000;
+    lastMills = currentMills;
+  };
   main(p);
 }, document.getElementById("app")!);
